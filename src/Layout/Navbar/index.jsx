8efc02e09bf7c0c923002024e99ef46c56e85185
AnarMuradov <mi82nr231@code.edu.az>
2023-12-21 19:58:33 +0400

    ws

---
+++ src/Layout/Navbar/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='navbar'>
+        <div className='navbarContainer'>
+        
+
+        </div>
+        
+
+    </nav>
+  )
+}
+
+export default Navbar
