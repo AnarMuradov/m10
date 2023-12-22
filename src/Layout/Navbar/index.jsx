2b9c1b4cf4916deb27b13bef799bc0b59f40c0b8
AnarMuradov <mi82nr231@code.edu.az>
2023-12-22 06:26:06 +0400

    navbar added

--- src/Layout/Navbar/index.jsx
+++ src/Layout/Navbar/index.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
-
+import './style.scss'
 const Navbar = () => {
   return (
     <nav className='navbar'>
         <div className='navbarContainer'>
-        
+        <div className='navbarContainer__logo'>
+          <img src="https://static.tildacdn.com/tild6263-6436-4064-b637-633061366565/M10.svg" alt="" />
+        </div>
+        <div className='navbarContainer__items'>
+          <ul className='navbarContainer__items__list'>
+            <li>PAY BY QR</li>
+            <li>SECURITY</li>
+            <li>FOR BUSINESS</li>
+          </ul>
+        </div>
+        <div className='navbarContainer__language'>
+          <select name="" id="">
+            <option value="">
+              EN
+            </option>
+            <option value="">
+              AZ
+            </option>
+            <option value="">
+              RU
+            </option>
+          </select>
+        </div>
 
         </div>
         
